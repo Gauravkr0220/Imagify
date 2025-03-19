f5f47d42ecf40dc4b51e89825e2e328028fc0f40
Gauravkr0220 <gauravkr0220@gmail.com>
2025-03-19 17:09:46 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import Header from './Header'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Descriptions from '../components/Descriptions'
+import Testimonials from '../components/Testimonials'
+import GenerateBtn from '../components/GenerateBtn'
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <Descriptions/>
+      <Testimonials/>
+      <GenerateBtn/>
     </div>
   )
 }
